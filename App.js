8d85efc66c7f9f9fd8b88b0b9b61352e9818bbbe
federico.torres <federico.torres@konecta.com.py>
2019-10-24 15:15:44 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function App() {
+  return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
